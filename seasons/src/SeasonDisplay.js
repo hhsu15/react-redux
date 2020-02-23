26c5fd6978df4c5ac43c2b1251d535ccf3c8164e
Hsinheng Hsu <hsin@Hsinhengs-MBP.fios-router.home>
2020-02-22 19:46:50 -0500

    prettier

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,14 +1,15 @@
-import './SeasonDisplay.css'
-import React from 'react'
+// @format
+import "./SeasonDisplay.css";
+
+import React from "react";
 
 const getSeason = (lat, month) => {
-
 	if (month > 2 && month < 9) {
 		return lat > 0 ? "summer" : "winter";
 	} else {
 		return lat > 0 ? "winter" : "summer";
 	}
-}
+};
 
 const SeasonDisplay = props => {
 	const seasonConfig = {
@@ -20,21 +21,20 @@ const SeasonDisplay = props => {
 			text: "Burr it's chilly",
 			iconName: "snowflake"
 		}
-	
-	}
+	};
 	const season = getSeason(props.lat, new Date().getMonth());
 
 	// this is using sementic ui. Check the documentation for available icons and how to use them
-    const { text, iconName } = seasonConfig[season]
+	const { text, iconName } = seasonConfig[season];
 
 	// a good practice here to have the style class name matches your component
 	return (
 		<div className={`season-display ${season}`}>
-		    <i className={`icon-left massive ${iconName} icon`} />
+			<i className={`icon-left massive ${iconName} icon`} />
 			<h1>{text}</h1>
 			<i className={`icon-right massive ${iconName} icon`} />
 		</div>
-	)
-}
+	);
+};
 
-export default SeasonDisplay
+export default SeasonDisplay;
