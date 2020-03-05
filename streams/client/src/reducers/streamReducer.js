a93db8d45cbefb9d45a1d1b7c14b2183076e5e99
Hsinheng Hsu <hsin@Hsinhengs-MBP.fios-router.home>
2020-03-04 22:25:49 -0500

    more on the streams

---
+++ streams/client/src/reducers/streamReducer.js
@@ -0,0 +1,28 @@
+import _ from "lodash";
+
+import {
+  FETCH_STREAM,
+  FETCH_STREAMS,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM
+} from "../actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") }; // use spread operator to unpack the object. This is similar to ** in python to unpack dict. You can use for array too.
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+
+    case DELETE_STREAM:
+      return _.omit(state, action.payload.id);
+    default:
+      return state;
+  }
+};
